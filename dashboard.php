690511bbe44ec5ee2c873554b5e1db3b6fada825
Konstantin Rogovenko <app.dev.ua@gmail.com>
2016-08-30 18:10:53 +0300

    dashboard and pricing pages text added

--- dashboard.php
+++ dashboard.php
@@ -19,10 +19,41 @@
 
 	<div class="demo-card-wide mdl-card mdl-shadow--2dp">
 	  <div class="mdl-card__title">
-	    <h2 class="mdl-card__title-text">Welcome</h2>
+	    <h2 class="mdl-card__title-text">NotifyMe - keep your domains safe</h2>
 	  </div>
 	  <div class="mdl-card__supporting-text">
-	    Currently you have 2 domains that are being monitored and 1 unread notification.
+	    <p>
+	    	<b>What is NotifyMe?</b>
+	    </p>
+	    <p>
+	    	Get instant notifications about changes happened to your domain names*:
+	    	<ul>
+	    		<li>Domain status changes</li>	    		
+	    		<li>Registrant WHOIS contact changes</li>
+	    		<li>Admin WHOIS contact changes</li>
+	    		<li>DNS changes</li>
+	    		<li>Expiration date changes</li>
+	    	</ul>
+	    </p>
+	    <p>
+	    	<b>Why do you need it?</b>
+	    </p>
+	    <p>
+	    	There are a few reasons why people lose their domain names. The most common are:
+	    	<ul>
+	    		<li>Domain expiration</li>
+	    		<li>Domain hijacking</li>
+	    	</ul>
+			Prevent your domain names from expiration or being hijacked. If domain status or domain contacts have changed, it is possible that the domain name is being stolen at the moment. The sooner you know about it, the better chance you have to prevent the steal or to recover it (if domain was hijacked).**
+	    </p>
+	  <div class="mdl-card__supporting-text">
+	    <p>
+	    	<i>* - it may take up to 24 hours for us to detect the change due to delays in registry-registrar reporting and due to the unstable nature of registrar whois service</i>
+	    </p>
+	    <p>
+	    	<i>** - NotifyMe does not guarantee that monitored domains are 100% safe. You need to use other ways of domain protection, like strong passwords to your registrar accounts, 2FA, etc.</i>
+	    </p>
+	  </div>  
 	  </div>
 	  <div class="mdl-card__actions mdl-card--border">
 	    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
--- pricing.php
+++ pricing.php
@@ -9,7 +9,7 @@
             <style>
             .demo-card-square.mdl-card {
               width: 300px;
-              height: 200px;
+              height: 250px;
             }
             .demo-card-square > .mdl-card__title {
               color: #fff;
@@ -22,8 +22,13 @@
                 <h2 class="mdl-card__title-text">Basic</h2>
               </div>
               <div class="mdl-card__supporting-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenan convallis.
+              Description:
+                <ul>
+                  <li>Costs $1 per month</li>
+                  <li>Up to 30 domains</li>
+                  <li>History of changes</li>
+                  <li>Email notifications</li>
+                </ul>
               </div>
               <div class="mdl-card__actions mdl-card--border">
                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
@@ -51,8 +56,13 @@
                 <h2 class="mdl-card__title-text">Standard</h2>
               </div>
               <div class="mdl-card__supporting-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenan convallis.
+                Description:
+                <ul>
+                  <li>Costs $3 per month</li>
+                  <li>Up to 100 domains</li>
+                  <li>History of changes</li>
+                  <li>Email notifications</li>
+                </ul>
               </div>
               <div class="mdl-card__actions mdl-card--border">
                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
@@ -67,7 +77,7 @@
             <style>
             .demo-card-square.mdl-card {
               width: 300px;
-              height: 200px;
+              height: 280px;
             }
             .demo-card-square > .mdl-card__title {
               color: #fff;
@@ -80,8 +90,14 @@
                 <h2 class="mdl-card__title-text">Premium</h2>
               </div>
               <div class="mdl-card__supporting-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Aenan convallis.
+                Description:
+                <ul>
+                  <li>Costs $6 per month</li>
+                  <li>Up to 150 domains</li>
+                  <li>History of changes</li>
+                  <li>Email notifications</li>
+                  <li>SMS notifications</li>
+                </ul>
               </div>
               <div class="mdl-card__actions mdl-card--border">
                 <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
